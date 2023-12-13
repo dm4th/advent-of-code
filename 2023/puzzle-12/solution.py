824a0b743ccc262b47158fd304582ffc12dfecb0
Danny Mathieson <danny.mathieson@ta-fvfg81azq05f.lan>
2023-12-12 20:56:25 -0800

    struggling with 12

--- 2023/puzzle-12/solution.py
+++ 2023/puzzle-12/solution.py
@@ -6,8 +6,32 @@ from typing import List, Union
 import math
 import concurrent.futures
 
-def parse_schematic(schematic_str: str, mapping: List[int]):
-    broken_count = sum(mapping)
+def count_char(input_str: str, char_list: List[str]) -> int:
+    ret_val = 0
+    for s in input_str:
+        if s in char_list:
+            ret_val += 1
+    return ret_val
+
+def get_char_indices(input_str: str, char: str) -> List[int]:
+    ret_val = []
+    for s in range(len(input_str)):
+        if input_str[s] == char:
+            ret_val.append(s)
+    return ret_val
+
+def count_scenarios(schematic_str: str, mapping_int: int) -> int:
+    iterations = 2**count_char(schematic_str,['?'])
+    ret_val = 0
+    while iterations > 0:
+        return 0
+
+
+    
+
+def parse_schematic(schematic_str: str, mapping: List[int]) -> int:
+    pass
+    
 
 def get_variations(schematic: List[str], mapping: List[int]) -> int:
     if len(schematic) == 0:
@@ -15,11 +39,18 @@ def get_variations(schematic: List[str], mapping: List[int]) -> int:
     
     elif len(schematic) == 1:
         if len(mapping) == 1:
-            return 2**(len(mapping) - mapping[0])
+            return count_scenarios(schematic[0], mapping[0])
         else:
             return parse_schematic(schematic[0], mapping)
         
     else:
+        # get the potential number of broken pieces in the first schematic
+        schematic_0 = schematic.pop(0)
+        potential_broken = count_char(schematic_0, ['?','#'])
+        if potential_broken == mapping[0]:
+            return 0
+
+
         
 
 
@@ -28,6 +59,9 @@ def calculate_variations(input_str: str) -> int:
     input_split = input_str.split(' ')
     schematic = input_split[0].split('.')
     mapping = input_split[1].split(',')
+    mapping = [int(m) for m in mapping]
+
+    print(schematic, mapping)
 
     return get_variations(schematic, mapping)
 
@@ -38,7 +72,7 @@ def part_1():
 
     variations = []
     with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
-        results = [executor.submit(calculate_variations, input_str) for input_str in input_lines]
+        results = [executor.submit(calculate_variations, input_str.strip()) for input_str in input_lines]
 
         for future in concurrent.futures.as_completed(results):
             variations.append(future.result())
